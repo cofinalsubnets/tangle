365fa79e6530f1c87a6dd4bef7a859e662d41d5b
Darius Bacon <darius@wry.me>
2013-02-23 13:43:04 -0500

    order defs top-down

--- Tangle.hs
+++ Tangle.hs
@@ -16,19 +16,6 @@ type WordMap = TM.TrieMap String Counter
 mangle :: Int -> String -> [String] -> StdGen -> [String]
 mangle n = wStream . train n
 
--- | Rotate lst to the left by n places.
-rot :: Int -> [a] -> [a]
-rot n lst = t ++ h
-  where (h,t) = splitAt n' lst
-        n'    = n `mod` length lst
-
--- | Split a list into non-overlapping sublists of size n (except possibly for
--- the last sublist)
-slices :: Int -> [a] -> [[a]]
-slices _ [] = []
-slices n ls = s : (slices n l)
-  where (s, l) = splitAt n ls
-
 -- | Return a word n-gram Markov model for str.
 train :: Int -> String -> WordMap
 train n str = foldl count TM.mkTrie groups
@@ -36,13 +23,33 @@ train n str = foldl count TM.mkTrie groups
         count d ws = TM.insert (init ws) (TC.insert (last ws) d') d
           where d' = fromMaybe TC.mkTrie $ TM.lookup (init ws) d
 
-rots :: [a] -> Int -> [[a]]
-rots ws n = [rot i ws | i <- [0..n-1]]
-
 -- | Return the length-n sublists of ws, in order.
 groupWs :: [a] -> Int -> [[a]]
 groupWs ws n = filter (\s -> length s == n) $ concatMap (slices n) $ rots ws n
         
+-- | Split a list into non-overlapping sublists of size n (except possibly for
+-- the last sublist)
+slices :: Int -> [a] -> [[a]]
+slices _ [] = []
+slices n ls = s : (slices n l)
+  where (s, l) = splitAt n ls
+
+rots :: [a] -> Int -> [[a]]
+rots ws n = [rot i ws | i <- [0..n-1]]
+
+-- | Rotate lst to the left by n places.
+rot :: Int -> [a] -> [a]
+rot n lst = t ++ h
+  where (h,t) = splitAt n' lst
+        n'    = n `mod` length lst
+
+-- | Generate a random sequence from a model and a starting state.
+wStream :: WordMap -> [String] -> StdGen -> [String]
+wStream dict state rng = case select (nxts state dict) rng of
+  Nothing -> []
+  Just (nxt,rng') -> nxt:(wStream dict state' rng')
+    where state' = (init state) ++ [nxt]
+
 -- | Return the weighted options for the next output from a state.
 nxts :: [String] -> WordMap -> [(String,Int)]
 nxts [] _ = []
@@ -58,11 +65,3 @@ select opts rng = Just (snd $ foldl1 sel opts', rng')
         opts' = wts `zip` map fst opts
         sel s s' = if fst s' > n then s else s'
         (n, rng') = randomR (1,last wts) rng
-
--- | Generate a random sequence from a model and a starting state.
-wStream :: WordMap -> [String] -> StdGen -> [String]
-wStream dict state rng = case select (nxts state dict) rng of
-  Nothing -> []
-  Just (nxt,rng') -> nxt:(wStream dict state' rng')
-    where state' = (init state) ++ [nxt]
-

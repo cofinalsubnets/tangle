c13dafa6567f355290edff3ba3558db541a00cf4
Darius Bacon <darius@wry.me>
2013-02-23 13:45:16 -0500

    simpler function for sublists

--- Tangle.hs
+++ Tangle.hs
@@ -1,6 +1,7 @@
 module Tangle (mangle) where
 
 import System.Random
+import Data.Array
 import Data.Maybe
 import qualified Trie.Count as TC
 import qualified Trie.Map as TM
@@ -19,29 +20,15 @@ mangle n = wStream . train n
 -- | Return a word n-gram Markov model for str.
 train :: Int -> String -> WordMap
 train n str = foldl count TM.mkTrie groups
-  where groups = concat [groupWs (words str) i | i <- [2..n+1]]
+  where groups = concat [sublists i (words str) | i <- [2..n+1]]
         count d ws = TM.insert (init ws) (TC.insert (last ws) d') d
           where d' = fromMaybe TC.mkTrie $ TM.lookup (init ws) d
 
--- | Return the length-n sublists of ws, in order.
-groupWs :: [a] -> Int -> [[a]]
-groupWs ws n = filter (\s -> length s == n) $ concatMap (slices n) $ rots ws n
-        
--- | Split a list into non-overlapping sublists of size n (except possibly for
--- the last sublist)
-slices :: Int -> [a] -> [[a]]
-slices _ [] = []
-slices n ls = s : (slices n l)
-  where (s, l) = splitAt n ls
-
-rots :: [a] -> Int -> [[a]]
-rots ws n = [rot i ws | i <- [0..n-1]]
-
--- | Rotate lst to the left by n places.
-rot :: Int -> [a] -> [a]
-rot n lst = t ++ h
-  where (h,t) = splitAt n' lst
-        n'    = n `mod` length lst
+-- | Return the length-k sublists of xs, in order.
+sublists :: Int -> [a] -> [[a]]
+sublists k xs = [map (a!) [i..i+k-1] | i <- [0..n-k]]
+  where a = listArray (0, n-1) xs
+        n = length xs
 
 -- | Generate a random sequence from a model and a starting state.
 wStream :: WordMap -> [String] -> StdGen -> [String]

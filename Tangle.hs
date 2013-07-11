fb6c36e3b5715def25bd4d73f99ef277c400725c
feivel <walpurgisriot@gmail.com>
2013-07-11 15:23:17 -0400

    make code more self-documenting

--- Tangle.hs
+++ Tangle.hs
@@ -1,40 +1,39 @@
 module Tangle (mangle) where
 
-import Data.Map (Map)
 import System.Random (RandomGen, randomR)
 import Data.List (tails)
 import qualified Data.Map as M
 
-type Model a = Map [a] (Map a Int)
+type Model a = M.Map [a] (M.Map a Int)
 
 -- | Generate a novel text based on an input text by resolving the input
 -- into a Markov model with transition memory at most n and following the chain.
 mangle :: (Ord a, RandomGen g) => Int -> [a] -> g -> [a]
-mangle n ws = maybe [] start . initialState
+mangle n items = maybe [] go . initialState
   where
-    start = uncurry $ chain (model n ws)
-    initialState = sample $ n `grams` ws
+    go = uncurry $ chain (model n items)
+    initialState = sample (n `grams` items)
 
 -- | Build an order i model from a sequence of states.
 model :: Ord a => Int -> [a] -> Model a
-model i ws = train M.empty ngrams
+model n items = train M.empty ngrams
   where
     train = foldl $ \m l -> transition m (init l) (last l)
-    ngrams = concat [n `grams` ws | n <- [2..i]]
+    ngrams = concatMap (flip grams items) [2..n]
 
 chain :: (Ord a, RandomGen g) => Model a -> [a] -> g -> [a]
-chain m st = maybe [] continue . sample candidates
+chain mdl st = maybe [] next . sample candidates
   where
-    continue (n, rng) = n : chain m (tail st ++ [n]) rng
-    candidates = expand (successors m st)
-    expand = concatMap (uncurry $ flip replicate)
+    next (n,r) = n : chain mdl (tail st ++ [n]) r
+    candidates = expand (successors mdl st)
+    expand = concatMap $ \(a,b) -> replicate b a
 
 -- | Train a model with a state transition.
 transition :: Ord a => Model a -> [a] -> a -> Model a
-transition m s e = M.insert s (M.insert e count counts) m
+transition mdl st ev = M.insert st (M.insert ev count counts) mdl
   where
-    count  = succ $ M.findWithDefault 0 e counts
-    counts = M.findWithDefault M.empty s m
+    count  = succ $ M.findWithDefault 0 ev counts
+    counts = M.findWithDefault M.empty st mdl
 
 -- | All length-n sublists.
 grams :: Int -> [a] -> [[a]]
@@ -42,7 +41,7 @@ grams n = filter ((==n) . length) . map (take n) . tails
 
 -- | Return the weighted options for the next output from a state.
 successors :: Ord a => Model a -> [a] -> [(a, Int)]
-successors d = concatMap (maybe [] M.toList . flip M.lookup d) . tails
+successors mdl = concatMap (maybe [] M.toList . flip M.lookup mdl) . tails
 
 -- | Sample a random element from a list.
 sample :: RandomGen g => [a] -> g -> Maybe (a,g)
